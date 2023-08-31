b4027e37ae882948242099eb97e99df765da3cfc
Marcos <markitosanches@gmail.com>
2023-08-31 12:07:44 -0400

    seance 6

---
+++ my-blog/app/Http/Controllers/BlogPostController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BlogPost;
+use Illuminate\Http\Request;
+
+class BlogPostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //SELECT * FROM `blog_posts` 
+        $posts = BlogPost::all();
+        return view('blog.index', ['posts'=>$posts]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\BlogPost  $blogPost
+     * @return \Illuminate\Http\Response
+     */
+    public function show(BlogPost $blogPost)
+    {
+        //$blogPost = SELECT * FROM `blog_posts` WHERE id = $blogPost;
+        //$stmt = $connex->prepare(SELECT * FROM `blog_posts` WHERE id = ?)
+        //$stmt->execute(array(10));
+        //$stmt-fetch();
+                
+        return $blogPost;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\BlogPost  $blogPost
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(BlogPost $blogPost)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\BlogPost  $blogPost
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, BlogPost $blogPost)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\BlogPost  $blogPost
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(BlogPost $blogPost)
+    {
+        //
+    }
+}
---
+++ my-blog/resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ config('app.name')}} - @yield('title')</title>
+    <!--Bootstrap CSS CDN-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 pt-4">
+                <h1 class="display-3 mt-5">
+                    {{ config('app.name')}}
+                </h1>
+            @yield('content')
+            </div>
+        </div>
+    </div>
+</body>
+    <!--Bootstrap JS CDN-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+</html>
